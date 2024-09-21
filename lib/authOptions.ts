76df55ac7e101c41878e38f2a4eee18a796d29fd
Sankhadip Roy <sankhadiproy23@gmail.com>
2024-09-21 23:37:21 +0530

    deployment error fix-4

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -9,7 +9,7 @@ export const authOptions: NextAuthOptions = {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
         }),
     ],
-    secret: process.env.NEXTAUTH_SECRET ?? "",
+    secret: process.env.NEXTAUTH_SECRET,
 
     session: {
         strategy: "jwt", // JWT is the default, but you can explicitly declare it
@@ -21,11 +21,16 @@ export const authOptions: NextAuthOptions = {
                 httpOnly: true,
                 sameSite: "lax",
                 path: "/",
-                secure: process.env.NODE_ENV === "production",  // Set to true in production
+                secure: true,  // Set to true in production
             },
         },
     },
 
     debug: true, // log
 
+    callbacks: {
+        redirect({ url, baseUrl }) {
+            return url.startsWith(baseUrl) ? url : baseUrl;
+        },
+    },
 };
